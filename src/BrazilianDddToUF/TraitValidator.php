b18ad1f842e623d04574c3efd899496a9d61174f
Alisson <desenvolvimento@idealizetecnologia.com.br>
2019-02-08 17:56:42 -0200

    Alterado para não dar throw quando não for valido

--- src/BrazilianDddToUF/TraitValidator.php
+++ src/BrazilianDddToUF/TraitValidator.php
@@ -6,7 +6,7 @@ trait TraitValidator {
 
     protected static function validate(array $arrayDDD, $ddd){
         if( !array_key_exists($ddd, $arrayDDD) ) {
-            throw new \InvalidArgumentException('DDD inexistente');
+            return false;
         }
 
         return true;
